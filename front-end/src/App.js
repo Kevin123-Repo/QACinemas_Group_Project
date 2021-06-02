cb0a82816dd2cc7d1ea429b32092f0b837337c49
Kevin123-QA <KTse@qa.com>
2021-06-02 14:57:55 +0100

    KT: Created Router structure

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
 import Homepage from './Components/Homepage';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import NavBar from './Components/Header_Footer/NavBar';
+import FooterBar from './Components/Header_Footer/FooterBar';
+import OpeningTimes from './Components/OpeningTimes/OpeningTimes'
+
 
 function App() {
   return (
-    <Homepage/>
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/OpeningTimes">
+            <OpeningTimes />
+          </Route>
+        </Switch>
+        <FooterBar />
+      </Router>
+    </>
+
   );
 }
 
