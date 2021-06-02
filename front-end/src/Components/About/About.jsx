9cd5091cd6b8ea63678a2306276a240fa34885c1
Kevin123-QA <KTse@qa.com>
2021-06-02 19:14:22 +0100

    KT: About page set up

---
+++ front-end/src/Components/About/About.jsx
@@ -0,0 +1,12 @@
+import { Container } from 'reactstrap';
+import AboutTable from './AboutTable';
+const About = () => {
+    return (
+        <Container>
+            <h1><center> <u> About </u> </center> </h1>
+            <AboutTable />
+
+        </Container>
+    )
+}
+export default About;
