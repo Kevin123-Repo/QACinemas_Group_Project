a2182ef8d33d7910d72bdceaf37276e6ea0761bf
JhonQA <JUrregoAgudelo@qa.com>
2021-06-02 12:52:51 +0100

    JU: Finished basic design or Getting there page.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,7 +9,7 @@ import Google_Map from './Components/Getting_There/Google_Map';
 function App() {
   return (
     <>
-    <h1> Welcome </h1>
+    
     {/* <Google_Map/> */}
     <Getting_There/>
     </>
--- front-end/src/Components/Getting_There/Getting_There.jsx
+++ front-end/src/Components/Getting_There/Getting_There.jsx
@@ -1,3 +1,5 @@
+
+
 import Cinema_Image from "../../Images/Everyman_cinema_Barnet.jpg"
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
@@ -8,20 +10,39 @@ const Getting_There = (props) => {
     return (
         <>
             <Container>
+                {/* Public Transport row and col */}
                 <Row>
-                    <Col><h1>Address</h1></Col>
-                    <Col><h1>Public Transport</h1>
-                    <p>Closest tube station is ...</p></Col>
-                    <Col><h1>Parking</h1>
-                   </Col>
-                    <Col><Google_Map/></Col>
+                    <Col ><h1>Public Transport</h1></Col>
+                </Row>
 
+                <Row>
+                    <Col><p>You'll find us at [address here]. You can get here by bus or by tube! The closest station is [tube station]</p>
+                    </Col>
+                </Row>
+
+                {/* Parking row and col */}
+                <Row>
+                    <Col lg="1"><h1>Parking</h1></Col>
+                </Row>
+
+                <Row>
+                    <Col><p>Our facilities has state of the art accessability. Parking included! All available parking spaces and disabled priority spaces will be at the front of the venue!</p>
+                     <img src={Cinema_Image} width = "40%" /></Col>
                 </Row>
 
 
+                {/* Address and Map row and col */}
                 <Row>
-                    <Col><img src={Cinema_Image} width="350" height="350"></img></Col>
+                    <Col lg="1"><h1>Address</h1></Col>
                 </Row>
+
+                <Row>
+                    <Col><p>You'll find us at [address here]. Please refer to the Map for further assistance!</p>
+                        <Google_Map /></Col>
+                </Row>
+
+
+
 
 
             </Container>
