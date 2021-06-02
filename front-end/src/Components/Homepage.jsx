4417d382ea60cd492d54b687ce709c1cf5231caf
raphyfqa <RFywell@qa.com>
2021-06-02 10:31:36 +0100

    RF: reformatted page structure

--- front-end/src/Components/Homepage.jsx
+++ front-end/src/Components/Homepage.jsx
@@ -6,15 +6,17 @@ const Homepage = () => {
     return(
         <div>
             <Row>
-                <Col lg="6">
+                <Col lg="1"></Col>
+                <Col lg="5">
                     
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore impedit cumque inventore cupiditate dolores? Quis et neque ipsam architecto totam recusandae possimus laboriosam dolores iste porro exercitationem, tenetur corrupti? Ipsam?
                     
                 </Col>
 
-                <Col lg="6">
+                <Col lg="5">
                     <img src={cinemaImage} alt="cinema screen"/>
                 </Col>
+                <Col lg="1"></Col>
             </Row>
             
         </div>
