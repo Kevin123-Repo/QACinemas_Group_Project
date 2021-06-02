5d78969c49bf55ed3db8dfedec3a2e6506b58a0a
georgeryalls <gryalls@qa.com>
2021-06-02 10:51:06 +0100

    GR: Reverted App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import OpeningTimes from './Components/OpeningTimes/OpeningTimes.jsx';
 
 function App() {
   return (
-    //<h1> Welcome </h1>
-    <>
-    <OpeningTimes/>
-    </>
+    <h1> Welcome </h1>
   );
 }
 
