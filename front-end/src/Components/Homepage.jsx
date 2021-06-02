d53c2ed089f430306a65dd8d0b2ff7786d06e09d
raphyfqa <RFywell@qa.com>
2021-06-02 11:23:28 +0100

    RF: added site purpose text

--- front-end/src/Components/Homepage.jsx
+++ front-end/src/Components/Homepage.jsx
@@ -8,8 +8,19 @@ const Homepage = () => {
             <Row>
                 <Col lg="1"></Col>
                 <Col lg="5">
+                    <h2>Welcome!</h2>
+                        <p>
+                            Welcome to QA Cinemas! Here you can browse all of the latest films, book tickets for showings in advance, and get a preview of new releases arriving in the near future!  
+                        </p>
+
+                    <br/>
+
+                    <h2>QA Cinemas</h2>
+                        <p>
+                            QA Cinemas is the UK's premiere multiplex cinema chain, and the best destination to kick back, relax, and forget your worries for a few hours. Grab some popcorn and an extra large soft drink of your choice and be transported to a new world by the magic of cinema. Take your family, friends, or even just that special someone... yourself!
+                        </p>
                     
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore impedit cumque inventore cupiditate dolores? Quis et neque ipsam architecto totam recusandae possimus laboriosam dolores iste porro exercitationem, tenetur corrupti? Ipsam?
+                    
                     
                 </Col>
 
