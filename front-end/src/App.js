6b95be2b5aa6521f9c62c34ab5e635fc8484737e
georgeryalls <gryalls@qa.com>
2021-06-02 10:23:35 +0100

    GR: Added opening times structure

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import OpeningTimes from './Components/OpeningTimes/OpeningTimes.jsx';
 
 function App() {
   return (
-    <h1> Welcome </h1>
+    //<h1> Welcome </h1>
+    <>
+    <OpeningTimes/>
+    </>
   );
 }
 
---
+++ front-end/src/Components/OpeningTimes/OpeningTimes.jsx
@@ -0,0 +1,22 @@
+import {Container} from 'reactstrap';
+const OpeningTimes = () =>{
+
+    return(
+        <Container>
+        <h1 align='center'>Opening Times</h1>
+        <div>
+        <h5 align='center'>Monday: 10:00 - 23:00</h5>
+        <h5 align='center'>Tuesday: 10:00 - 23:00</h5>
+        <h5 align='center'>Wednesday: 10:00 - 23:00</h5>
+        <h5 align='center'>Thursday: 10:00 - 23:00</h5>
+        <h5 align='center'>Friday: 10:00 - 00:00</h5>
+        <h5 align='center'>Saturday: : 10:00 - 00:00</h5>
+        <h5 align='center'>Sunday: 10:00 - 22:00</h5>
+      
+        </div>
+        </Container>
+    );
+
+};
+
+export default OpeningTimes;
