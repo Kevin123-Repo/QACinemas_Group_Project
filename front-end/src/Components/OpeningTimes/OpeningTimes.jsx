32da98c344dd6baa038fb6827a2c78c7d1be215d
JhonQA <JUrregoAgudelo@qa.com>
2021-06-07 15:51:22 +0100

    JU: Accordion drop down added for opening times and other questions

--- front-end/src/Components/OpeningTimes/OpeningTimes.jsx
+++ front-end/src/Components/OpeningTimes/OpeningTimes.jsx
@@ -7,14 +7,7 @@ const OpeningTimes = () =>{
         <Container className="times-div">
 
             <FaqAccordion/>
-            <h1 align='center'><img src = {Clock} height = '55' width = '55' alt = 'Clock'/> Opening Times <img src = {Clock} height = '55' width = '55' alt = 'Clock'/></h1>
-            <h5 align='center'>Monday: 10:00 - 23:00</h5>
-            <h5 align='center'>Tuesday: 10:00 - 23:00</h5>
-            <h5 align='center'>Wednesday: 10:00 - 23:00</h5>
-            <h5 align='center'>Thursday: 10:00 - 23:00</h5>
-            <h5 align='center'>Friday: 10:00 - 00:00</h5>
-            <h5 align='center'>Saturday: 10:00 - 00:00</h5>
-            <h5 align='center'>Sunday: 10:00 - 22:00</h5>
+            
         </Container>
     );
 
