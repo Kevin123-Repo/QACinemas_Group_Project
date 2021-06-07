3cddf0cfb65fc57e92a12b0c8ebf5d753cc511db
JhonQA <JUrregoAgudelo@qa.com>
2021-06-07 15:50:37 +0100

    JU: Styling the Opening times page, added a new module called FAQ accordion

---
+++ front-end/src/Components/OpeningTimes/FaqAccordion.jsx
@@ -0,0 +1,14 @@
+
+import { Accordion, Card } from 'react-bootstrap';
+import { NavLink as Link } from 'react-router-dom'
+
+const FaqAccordion = (props) => {
+  
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default FaqAccordion;
--- front-end/src/Components/OpeningTimes/OpeningTimes.jsx
+++ front-end/src/Components/OpeningTimes/OpeningTimes.jsx
@@ -1,9 +1,12 @@
 import {Container} from 'reactstrap';
 import Clock from '../../Images/Clock_QA_Colour.png';
+import FaqAccordion from './FaqAccordion';
 const OpeningTimes = () =>{
 
     return(
         <Container className="times-div">
+
+            <FaqAccordion/>
             <h1 align='center'><img src = {Clock} height = '55' width = '55' alt = 'Clock'/> Opening Times <img src = {Clock} height = '55' width = '55' alt = 'Clock'/></h1>
             <h5 align='center'>Monday: 10:00 - 23:00</h5>
             <h5 align='center'>Tuesday: 10:00 - 23:00</h5>
