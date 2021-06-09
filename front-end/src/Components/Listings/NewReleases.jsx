4374ae69b9b016ed579fd284f938e6e0cc901c76
JhonQA <JUrregoAgudelo@qa.com>
2021-06-09 17:19:27 +0100

    JU: Added Journey test for New Releases

--- front-end/src/Components/Listings/NewReleases.jsx
+++ front-end/src/Components/Listings/NewReleases.jsx
@@ -53,7 +53,7 @@ const NewReleases = () => {
             key={i}
           >
             <Link to={`/movies/${item.title}`}>
-                <img className={"listingImgs"} src={item.imageUrl} alt={""} />
+                <img className={"listingImgs"} src={item.imageUrl} alt={i} />
             </Link>
             <CarouselCaption captionText={item.title} />
           </CarouselItem>
